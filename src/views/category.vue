8f554d82f4a1d0d9cb08824da68170b5f8839063
fgh23333 <635262140@qq.com>
2023-07-23 13:17:41 +0800

    Update category.vue

--- src/views/category.vue
+++ src/views/category.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="category">
-    <h1>分类</h1>
+  <div id="category">
+    <div class="clear searchBar">
+      <div class="back left" @click="goBack">
+        <van-icon name="arrow-left" />
+      </div>
+      <div class="search left">
+        <div id="inputbox">
+          <van-icon name="search" />
+          <input type="search" id="search-in" placeholder="扫地机器人智能">
+        </div>
+      </div>
+    </div>
+    
+
     <bottomBar active="/category"></bottomBar>
   </div>
 </template>
@@ -11,6 +23,56 @@ import bottomBar from "../components/bottomBar.vue";
 export default {
   components: {
     bottomBar
+  },
+  methods: {
+    goBack() {
+      this.$router.go(-1)
+    }
   }
 }
 </script>
+
+<style lang="less">
+#category {
+  .searchBar {
+    height: 44px;
+    text-align: center;
+    width: 100%;
+    position: fixed;
+    
+    .back {
+      width: 50px;
+      line-height: 44px;
+    }
+
+    .left {
+      float: left;
+    }
+
+    .search {
+      height: 30px;
+      width: calc(100vw - 100px);
+      background-color: white;
+      margin: 7px 0;
+      border-radius: 15px;
+      background-color: #f7f7f7;
+    }
+
+    #inputbox {
+      line-height: 30px;
+      position: relative;
+      
+
+      #search-in {
+        border: none;
+        outline: none;
+        width: 75%;
+        display: inline;
+        left: 10px;
+        position: relative;
+        background-color: #f7f7f7;
+      }
+    }
+  }
+}
+</style>
